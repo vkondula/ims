b368caa2628968a5614183dd4be1da7d1546c4ce
vkondula <vkondula@redhat.com>
2016-11-30 01:23:06 +0100

    Basic flow

--- main.cc
+++ main.cc
@@ -11,6 +11,8 @@ using namespace std;
 vector<Zone *> zones;
 
 int main() {
+  RandomSeed(time(NULL));
+  srand(time(NULL));
   SetOutput("output.out");
   Init(TIME_BEGIN, TIME_END + 3 * HOUR);
   init_zones();
@@ -32,17 +34,84 @@ void init_zones(){
 }
 
 void Group::Behavior(){
-    this->timestamps.push_back(Time); // Save arrival
-    // Store * table = find_table();
-    // if (!table){
-    //     // TODO: odchazi nebo se rozdeluji
-    //     std::cout << "/* Group didn't get table */" << std::endl;
-    //     return;
-    // }
-    // Seize(TABLE, this->size);
-    // Wait(TIME_TO_SIT);
-    // Seize();
-
-
+  this->timestamps.push_back(Time); // Save arrival
+  Zone * zone = find_zone_with_table(this->size);
+  /* When zone with suitable table was found, the group owns the thread
+     so they can aquire the table exclusivly
+  */
+  if (!zone){
+      // TODO: odchazi nebo se rozdeluji
+      std::cout << "/* Group didn't get table */" << std::endl;
+      return;
+  }
+  Store * table = zone->find_table(this->size);
+  Enter(*table, this->size);
+  /* Taking a seat */
+  Wait(Uniform(TIME_TO_SIT_L, TIME_TO_SIT_H));
+  /* Waiting for soup and order */
+  zone->q.Insert(this);
+  Passivate();
+  Wait(TIME_TO_ORDER_GENERAL + Uniform(TIME_TO_ORDER_L, TIME_TO_ORDER_H) * this->size);
+  this->curr_waiter->Activate();
+  /* Eating soup */
+  std::cerr << "/* POLIVKA */" << std::endl;
+  Wait(Uniform(TIME_TO_EAT_SOUP_L,TIME_TO_EAT_SOUP_H));
+  /* Wating for waiter to take away dishes (soup) */
+  zone->q.Insert(this);
+  Passivate();
+  Wait(TIME_TO_CLEAN * this->size);
+  this->curr_waiter->Activate();
+  /* Waiting for meal */
+  zone->q.Insert(this);
+  Passivate();
+  Wait(TIME_TO_SERVER_MEAL * this->size);
+  this->curr_waiter->Activate();
+  /* Eating meal */
+  std::cerr << "/* HLAVNI CHOD */" << std::endl;
+  Wait(Uniform(TIME_TO_EAT_MEAL_L,TIME_TO_EAT_MEAL_H));
+  /* Wating for waiter to take away dishes (meal) and pay */
+  zone->q.Insert(this);
+  Passivate();
+  Wait(Uniform(TIME_TO_PAY_L, TIME_TO_PAY_H));
+  Wait(TIME_TO_CLEAN * this->size);
+  this->curr_waiter->Activate();
+  /* Leaving the restaurant */
+  std::cerr << "/* ODCHAZI */" << std::endl;
+  Leave(*table, this->size);
 }
-void Group::next_phase(){/*TODO*/}
+
+void Waiter::Behavior(){
+  while (true){
+      if (!this->zone->q.Empty()){
+        Group * group = (Group *) this->zone->q.GetFirst();
+        // TODO: cesta kuchyn -> zona
+        group->curr_waiter = this;
+        group->Activate();
+        Passivate();
+        // TODO: cesta z5
+      } else {
+        /* Wait for next request */
+        Wait(10);
+      }
+  }
+}
+
+void Waiter::set_zone(Zone * z){
+  this->zone = z;
+}
+
+Zone * find_zone_with_table(int size){
+  // TODO: zmenit poradit zon pro kazdou zkupinu
+  for (Zone * z : zones){
+    Store * s = z->find_table(size);
+    if(s) return z;
+  }
+  return NULL;
+}
+
+Store * Zone::find_table(int size){
+  for(Store * t : this->table){
+    if (t->Free() >= (unsigned int)size ) return t;
+  }
+  return NULL;
+}
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include "simlib.h"
 
 /* WORKING TIME DEFINITION */
@@ -15,6 +16,26 @@ const int TIME_END = 14 * HOUR;
 
 /* OPERATIONS DURATIONS */
 // TODO
+const int TIME_TO_SIT_L = 5;
+const int TIME_TO_SIT_H = 15;
+const int TIME_TO_SEARCH_ZONE_L = 10;
+const int TIME_TO_SEARCH_ZONE_H = 15;
+
+const int TIME_TO_ORDER_GENERAL = 8;
+const int TIME_TO_ORDER_L = 2;
+const int TIME_TO_ORDER_H = 7;
+
+const int TIME_TO_CLEAN = 3;
+const int TIME_TO_SERVER_MEAL = 4;
+
+const int TIME_TO_EAT_MEAL_L = 5 * MINUTE;
+const int TIME_TO_EAT_MEAL_H = 10 * MINUTE;
+const int TIME_TO_EAT_SOUP_L = 3 * MINUTE;
+const int TIME_TO_EAT_SOUP_H = 7 * MINUTE;
+
+const int TIME_TO_PAY_L = 10;
+const int TIME_TO_PAY_H = 20;
+
 
 /* ENVIRONMENT DESCRIPTION */
 
@@ -29,39 +50,54 @@ enum tPhase : int {
     WF_PAYMENT,
     LEAVING,
 };
+class Zone;
+
+class Waiter : public Process {
+  Zone * zone;
+  void Behavior();
+public:
+  Waiter(){ Activate(); }
+  void set_zone(Zone * z);
+};
 
 class Zone {
-    int waiter_count;
-    Facility * waiter;
-    std::vector<Store *> table;
-    Store find_table(int min_size);
-    public: Zone(int waiter_count, std::vector<int> tables_sizes){
-        this->waiter_count = waiter_count;
-        this->table.reserve(tables_sizes.size());
-        this->waiter= new Facility[waiter_count];
-        for (int i : tables_sizes){
-            Store * t = new Store();
-            t->SetCapacity(i);
-            this->table.push_back(t);
-        }
+  std::vector<Waiter *> waiter;
+  std::vector<Store *> table;
+public:
+  Zone(int waiter_count, std::vector<int> tables_sizes){
+    this->waiter.reserve(waiter_count);
+    this->table.reserve(tables_sizes.size());
+    for (int i : tables_sizes){
+      Store * t = new Store();
+      t->SetCapacity(i);
+      this->table.push_back(t);
     }
+    for (int i = 0; i<waiter_count; i++){
+      Waiter * w = new Waiter();
+      this->waiter.push_back(w);
+      w->set_zone(this);
+    }
+  }
+  Store * find_table(int min_size);
+  Queue q;
 };
 
 class Group : public Process {
-    int size;
-    tPhase phase;
-    Store table;
-    std::vector<int> timestamps;
-    Store find_table();
-    Group split_group();
-    void Behavior();
-    void next_phase();
-    public: Group(int size){
-      this->size = size;
-      this->timestamps.reserve(8);
-      this->Activate();
-      std::cout << "Group arrived at: " << Time << "\n";
-    }
+  int size;
+  tPhase phase;
+  Store table;
+  std::vector<int> timestamps;
+  void Behavior();
+public:
+  Group(int size){
+    this->size = size;
+    this->timestamps.reserve(8);
+    this->Activate();
+    std::cout << "Group arrived at: " << Time << "\n";
+  }
+  Waiter * curr_waiter;
+  Store find_table();
+  Group split_group();
 };
 
 class Generator : public Event {
@@ -74,5 +110,6 @@ class Generator : public Event {
 };
 
 void init_zones();
+Zone * find_zone_with_table(int size);
 
 #endif // MAIN_H
